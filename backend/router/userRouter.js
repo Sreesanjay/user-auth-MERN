d3e789f5d48cd23f51b2a7708936540cb4657b4c
Sreesanjay <sreesanjay7592@gmail.com>
2023-12-27 23:19:08 +0530

    remove

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -6,15 +6,15 @@ const userAuthController = require('../controller/userController')
 const {isUserLogedIn} = require('../middlewares/authMiddleware')
 
 const router = express.Router()
-router.post('/',(req,res) => {
-    console.log(req.body)
-    console.log('request got successfully')
-    res.json({
-        success: false,
-        message: 'server error'
-    })
+// router.post('/',(req,res) => {
+//     console.log(req.body)
+//     console.log('request got successfully')
+//     res.json({
+//         success: false,
+//         message: 'server error'
+//     })
     
-})
+// })
 router.post('/login', userAuthController.userLogin)
 router.post('/register-user', userAuthController.registerUser)
 router.post('/update-profile/:id',isUserLogedIn,upload.single('profile'),userAuthController.updateProfile)
